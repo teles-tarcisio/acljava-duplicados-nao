366e020dd55a09290f43083790a38f78b8aa5e01
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2023-03-21 13:34:28 -0300

    started repo

---
+++ src/DuplicadosNao.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class DuplicadosNao {
+    public static void main(String[] args) {
+        List<String> array1 = new ArrayList<>(List.of("Morango", "Uva", "Acerola", "Manga", "Pitomba"));
+        List<String> array2 = new ArrayList<>(List.of("Pera", "Caju", "Pitomba", "Morango", "Kiwi", "Cajá", "Morango"));
+
+        /*
+        List<String> array1 = new ArrayList<>();
+        array1.add("Morango");
+        array1.add("Uva");
+        array1.add("Acerola");
+        */      
+
+        List<String> result = new ArrayList<>();
+        
+        for (int i = 0; i < array1.size(); i++) {
+            if (array2.contains(array1.get(i))) {
+                result.add(array1.get(i));
+            }
+        }
+        System.out.println(result);
+
+    }
+}
